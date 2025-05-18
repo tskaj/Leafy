56523448bc1d4de80ef666dfcd4f8094f75ba43a
Abdullah Javed <126407499+tskaj@users.noreply.github.com>
2025-05-18 18:30:06 +0500

    Update roboflow_validation.py

--- backend/disease_detection/roboflow_validation.py
+++ backend/disease_detection/roboflow_validation.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from inference_sdk import InferenceHTTPClient
 
 # Initialize the Roboflow client with API key and URL
-ROBOFLOW_API_KEY = "xo6mQ5uBlOugUjY9G6ei"
+ROBOFLOW_API_KEY = os.getenv("ROBOFLOW_API_KEY")
 ROBOFLOW_API_URL = "https://serverless.roboflow.com"
 CLIENT = InferenceHTTPClient(api_url=ROBOFLOW_API_URL, api_key=ROBOFLOW_API_KEY)
 
